a71b660165e28db9111b4f70787460f183d8c940
Andres Reyes <andrespk3000@gmail.com>
2026-08-13 18:25:07 -0500

    project creation and update completed, missing content creation

--- my_apps/portafolios/urls.py
+++ my_apps/portafolios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from my_apps.portafolios.views import (
+    ProjectContentCreateView,
     ProjectContentUpdateView,
     ProjectCreateView,
     ProjectDetailView,
@@ -17,16 +18,6 @@ urlpatterns = [
         name="project_list",
     ),
     path(
-        "projects/<slug:slug>/",
-        ProjectDetailView.as_view(),
-        name="project_detail",
-    ),
-    path(
-        "projects/<slug:slug>/content/<int:pk>/edit/",
-        ProjectContentUpdateView.as_view(),
-        name="content_edit",
-    ),
-    path(
         "projects/create/",
         ProjectCreateView.as_view(),
         name="project_create",
@@ -35,5 +26,20 @@ urlpatterns = [
         "projects/<slug:slug>/edit/",
         ProjectUpdateView.as_view(),
         name="project_update",
+    ),
+    path(
+        "projects/<slug:slug>/",
+        ProjectDetailView.as_view(),
+        name="project_detail",
+    ),
+    path(
+        "projects/<slug:slug>/content/create/",
+        ProjectContentCreateView.as_view(),
+        name="content_create",
+    ),
+    path(
+        "projects/<slug:slug>/content/<int:pk>/edit/",
+        ProjectContentUpdateView.as_view(),
+        name="content_edit",
     ),
 ]
--- my_apps/portafolios/views.py
+++ my_apps/portafolios/views.py
@@ -8,7 +8,7 @@ from my_apps.portafolios.models import Project, ProjectContent
 
 
 class ProjectListView(ListView):
-    template_name = "portafolios/list.html"
+    template_name = "portafolios/projects/list.html"
     model = Project
     context_object_name = "projects"
 
@@ -17,10 +17,76 @@ class ProjectListView(ListView):
         return context
 
 
+# WORKING ON THIS
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectForm
+    template_name = "portafolios/projects/create.html"
+
+    def get_success_url(self):
+        return reverse("portafolio:project_list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["content_form"] = ProjectContentForm()
+        return context
+
+    def form_valid(self, form):
+        content_form = ProjectContentForm(
+            self.request.POST,
+            self.request.FILES,
+        )
+
+        if not content_form.is_valid():
+            context = self.get_context_data(form=form)
+            context["content_form"] = content_form
+
+            return self.render_to_response(context)
+
+        response = super().form_valid(form)
+
+        content = content_form.save(commit=False)
+        content.project = self.object
+        content.save()
+
+        return response
+
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    form_class = ProjectForm
+    template_name = "portafolios/projects/update.html"
+    context_object_name = "project"
+
+    def get_success_url(self):
+        return reverse(
+            "portafolio:project_detail",
+            kwargs={"slug": self.object.slug},
+        )
+
+
 class ProjectDetailView(DetailView):
     model = Project
-    template_name = "portafolios/detail.html"
+    template_name = "portafolios/projects/detail.html"
     context_object_name = "project"
+
+
+# PROJECT CONTENT MANAGMENT -- DO THIS AFTER FINISHING THE PROJECT CREATION
+
+
+class ProjectContentCreateView(CreateView):
+    model = ProjectContent
+    form_class = ProjectContentForm
+    template_name = "portafolios/content/create.html"
+    context_object_name = "content"
+
+    def get_success_url(self):
+        return reverse(
+            "portafolio:project_detail",
+            kwargs={"slug": self.object.project.slug},
+        )
 
 
 class ProjectContentUpdateView(UpdateView):
@@ -34,52 +100,3 @@ class ProjectContentUpdateView(UpdateView):
             "portafolio:project_detail",
             kwargs={"slug": self.object.project.slug},
         )
-
-
-# WORKING ON THIS
-
-
-class ProjectCreateView(CreateView):
-    model = Project
-    form_class = ProjectForm
-    template_name = "portafolios/create.html"
-
-    def get_success_url(self):
-        return reverse("portafolio:project_list")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["content_form"] = ProjectContentForm()
-        return context
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        content_form = ProjectContentForm(self.request.POST)
-        if content_form.is_valid():
-            content = content_form.save(commit=False)
-            content.project = self.object
-            content.save()
-        return response
-
-
-class ProjectUpdateView(UpdateView):
-    model = Project
-    form_class = ProjectForm
-    template_name = "portafolios/update.html"
-
-    def get_success_url(self):
-        return reverse("portafolio:project_list")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["content_form"] = ProjectContentForm()
-        return context
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        content_form = ProjectContentForm(self.request.POST)
-        if content_form.is_valid():
-            content = content_form.save(commit=False)
-            content.project = self.object
-            content.save()
-        return response
