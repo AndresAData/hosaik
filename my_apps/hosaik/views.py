0994eed6834028b8be67befb29bd8b997050dd77
Andres Reyes <andrespk3000@gmail.com>
2026-08-19 18:43:48 -0500

    correction on index view, we added the limit for 3 projects and now the data is comming fomr data base

--- my_apps/hosaik/views.py
+++ my_apps/hosaik/views.py
@@ -1,7 +1,15 @@
 from django.views.generic import TemplateView
 
 # Create your views here.
+from my_apps.portafolios.models import Project
 
 
 class IndexView(TemplateView):
     template_name = "hosaik/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["projects"] = Project.objects.order_by("-created_at")[:3]
+
+        return context
